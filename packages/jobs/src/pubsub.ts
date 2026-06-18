888c3452419d88491b410e955d5d5f2ea864f37f
Ahmad Chairiansyah <ahmadchairiansyah@RMacAir.local>
2026-06-18 15:25:36 +0700

    fix: handle ioredis EPIPE errors gracefully

--- packages/jobs/src/pubsub.ts
+++ packages/jobs/src/pubsub.ts
@@ -3,7 +3,9 @@ import { connection } from './queue';
 
 // @ts-ignore
 export const redisPublisher = new Redis(connection);
+redisPublisher.on('error', (err) => console.error('[Redis Publisher] Error:', err.message));
 
 // Subscriber client
 // @ts-ignore
 export const redisSubscriber = new Redis(connection);
+redisSubscriber.on('error', (err) => console.error('[Redis Subscriber] Error:', err.message));
